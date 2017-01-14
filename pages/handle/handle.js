c543bbf4c68e0ea80cd187d823938579c685e08c
yangyong <915477248@qq.com>
2017-01-14 08:36:41 +0800

    init

---
+++ pages/handle/handle.js
@@ -0,0 +1,14 @@
+//获取应用实例
+var app = getApp()
+Page({
+  data:{
+    title:''
+  },
+  onLoad:function(options){
+    // 改变title
+    // wx.setNavigationBarTitle({
+    //     title:'哈哈人'
+    // })
+  },
+  
+})
