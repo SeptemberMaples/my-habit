b537234b43e750e9034c7f1efc5e7c0623c3ccd7
yangyong <915477248@qq.com>
2017-02-16 16:03:44 +0800

    签到

--- dist/pages/handle/handle.js
+++ dist/pages/handle/handle.js
@@ -3,8 +3,20 @@
 var app = getApp();
 Page({
   data: {
-    title: ''
+    title: '',
+    iconSize: '45',
+    isSign: false
   },
-  onLoad: function onLoad(options) {}
+  onLoad: function onLoad(options) {
+    wx.setNavigationBarTitle({
+      title: '哈哈人'
+    });
+  },
 
+  signIn: function signIn() {
+    var that = this;
+    this.setData({
+      isSign: true
+    });
+  }
 });
--- src/pages/handle/handle.js
+++ src/pages/handle/handle.js
@@ -2,13 +2,23 @@
 var app = getApp()
 Page({
   data:{
-    title:''
+    title:'',
+    iconSize: '45',
+    isSign: false
   },
   onLoad:function(options){
     // 改变title
-    // wx.setNavigationBarTitle({
-    //     title:'哈哈人'
-    // })
+    wx.setNavigationBarTitle({
+        title:'哈哈人'
+    })
   },
-  
+
+  // 签到
+  signIn(){
+    var that = this
+    this.setData({
+      isSign: true
+    })
+  }
+
 })
